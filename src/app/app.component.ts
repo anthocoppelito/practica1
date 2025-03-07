0b28a113edb792a8938acd71bc38480f23725e57
anthocoppelito <practicante.dps@coppel.com>
2025-03-07 13:01:33 -0700

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InicioSesionComponent } from "./inicio-sesion/inicio-sesion.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InicioSesionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
