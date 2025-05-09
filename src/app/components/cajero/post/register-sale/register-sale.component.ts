2eeb9a688532b33642f25a9a8d400dcdf57d8762
anthocoppelito <practicante.dps@coppel.com>
2025-05-09 11:22:47 -0700

    se añadio la opcion de mostrar precio del producto y total segun la cantidad

--- src/app/components/cajero/post/register-sale/register-sale.component.ts
+++ src/app/components/cajero/post/register-sale/register-sale.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Form, FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { SaleService } from '../../../../services/sale/sale.service';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
 
 @Component({
   selector: 'app-register-sale',
@@ -18,6 +20,7 @@ export class RegisterSaleComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
+    private http: HttpClient,
     private router: Router,
     private _apiSale: SaleService,
   ){
@@ -44,22 +47,56 @@ export class RegisterSaleComponent implements OnInit {
     this.addProduct();
   }
 
-  //getter para acceder al formarray
-  get products(): FormArray {
-    return this.registerForm.get('products') as FormArray;
-  }
-
   // Método para agregar un nuevo producto al FormArray
   addProduct(): void {
     const productGroup = this.formBuilder.group({
-      productname: ['', Validators.required], // Nombre del producto
-      amount: ['', [Validators.required, Validators.pattern(/^[0-9]*$/)]], // Cantidad (solo números)
-      price: ['', [Validators.required, Validators.pattern(/^\d+(\.\d{1,2})?$/)]], // Precio (número con decimales)
-      total: ['', [Validators.required, Validators.pattern(/^\d+(\.\d{1,2})?$/)]] // Total (número con decimales)
+      productname: [''], // Nombre del producto
+      amount: [''], // Cantidad (solo números)
+      price: [''], //solo lectura
+      total: [''] // solo lectura
     });
+
+    // Detectar cambios en los campos productname y amount
+    productGroup.valueChanges
+    .pipe(
+      debounceTime(2000), // Esperar 2 segundos después de que el usuario deje de escribir
+      distinctUntilChanged() // Evitar llamadas repetidas si los valores no cambian
+    )
+    .subscribe((values) => {
+      const { productname, amount } = values;
+      if (productname && amount) { // si ambos están presentes
+        this.fetchPriceAndTotal(productname, amount, productGroup);
+      }
+    });
+
+    
 
     this.products.push(productGroup);
   }
+
+  //getter para acceder al formarray
+  get products(): FormArray {
+    // return this.registerForm.get('products') as FormArray;
+    return this.registerForm.get('products') as FormArray;
+  }
+
+  // Método para consultar el precio y total desde el backend
+  fetchPriceAndTotal(productname: string, amount: string, productGroup: FormGroup): void {
+
+    this._apiSale.getSalePriceByProductAmount(productname, Number(amount)).subscribe({
+      next: (data) => {
+        productGroup.patchValue({
+          price: data.unitPrice,
+          total: data.totalPrice
+        });
+      },
+      error: (err) => {
+        console.error('Error al obtener el precio y total:', err);
+      }
+    });
+  }
+
+
 
   // Método para registrar la venta
   register(): void {
@@ -78,13 +115,7 @@ export class RegisterSaleComponent implements OnInit {
     }
   }
 
-  get productname() {
-    return this.registerForm.get('productname');
-  }
-  get amount() {
-    return this.registerForm.get('amount');
-  }
-  get price() {
-    return this.registerForm.get('price');
-  }
+
+
+
 }
--- src/app/services/sale/sale.service.ts
+++ src/app/services/sale/sale.service.ts
@@ -21,4 +21,9 @@ export class SaleService {
   getSaleById(saleid: number): Observable<Sale> {
     return this.http.get<Sale>(environment.urlApi+"sales/cajero/sale/"+saleid);
   }
+
+  //GetProductPriceByProductAmount - obtiene el precio unitario del producto y el total segun la cantidad
+  getSalePriceByProductAmount(productname: string, amount: number): Observable<{ unitPrice: number; totalPrice: number }> {
+    return this.http.get<{ unitPrice: number; totalPrice: number }>(environment.urlApi+"product/bodega/price/"+productname+"/"+amount);
+  }
 }
